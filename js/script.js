372201af7d59a794d92e18207121937a94a1d236
Nafisa Binta Tofayel <nafisa1805@cseku.ac.bd>
2024-09-19 13:16:15 +0600

    add money done

--- js/script.js
+++ js/script.js
@@ -4,10 +4,24 @@
 // step 1 -  set event handler
 document.getElementById('btn-login').addEventListener('click' , function(event){
     // step 2 - set prevent default (reloading browser)
-    event.preventDefault
+    event.preventDefault()
     console.log("login clicked")
 
-    const phoneNumber = document.getElementById('phn-number')
-    console.log(phoneNumber.value)
+    // step -3 get phone number and pin number
+    const phoneNumber = document.getElementById('phn-number').value
+    const pinNumber = document.getElementById('pin-number').value
+
+
+    // step - 4 validate data
+    if(phoneNumber === '5' && pinNumber === '1234'){
+        console.log('you are logged in')
+        window.location.href = '/home.html'
+    }
+    else{
+        alert('please input correct number')
+    }
 
 })
+
+
+// home page js starts here
